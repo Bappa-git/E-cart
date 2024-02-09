d65886ce0f4990d617c9ba739b78cd8d13db44f0
Bappa-git <bappadas09012000@gmail.com>
2024-02-09 23:30:28 +0530

    upload

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ const App = () => {
     <Router>
     <Navbar cart={cart} setData={setData} />
     <Routes>
-      <Route path="/" element={<><Owlcarousel/><Product cart={cart} setCart={setCart} items={data} /></>} />
+      <Route path="/" element={<><Product cart={cart} setCart={setCart} items={data} /></>} />
       <Route path="/product/:id" element={<ProductDetail cart={cart} setCart={setCart} />} />
       <Route path="/search/:term" element={<SearchItem cart={cart} setCart={setCart} />} />
       <Route path="/cart" element={<Cart cart={cart} setCart={setCart} />} />
