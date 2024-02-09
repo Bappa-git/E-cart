adcc3816ec776da9bd24cf71283ac9743c030a20
Bappa-git <bappadas09012000@gmail.com>
2024-02-09 19:40:52 +0530

    upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 import Product from './components/Product'
 import {BrowserRouter as Router, Routes,Route} from 'react-router-dom'
 import ProductDetail from './components/ProductDetail'
 import SearchItem from './components/SearchItem'
 import Cart from './components/Cart'
 import { items } from './components/Data'
+import Owlcarousel from './components/Owlcarousel'
 
 
 const App = () => {
@@ -16,11 +18,12 @@ const App = () => {
     <Router>
     <Navbar cart={cart} setData={setData} />
     <Routes>
-      <Route path="/" element={<Product cart={cart} setCart={setCart} items={data} />} />
+      <Route path="/" element={<><Owlcarousel/><Product cart={cart} setCart={setCart} items={data} /></>} />
       <Route path="/product/:id" element={<ProductDetail cart={cart} setCart={setCart} />} />
       <Route path="/search/:term" element={<SearchItem cart={cart} setCart={setCart} />} />
       <Route path="/cart" element={<Cart cart={cart} setCart={setCart} />} />
     </Routes>
+    <Footer/>
   
     </Router>
     </>
